f2ba3ac8da3eb088740b67235e932cf2cb5587f6
Alejandro Ruiz <alejandroruizponce93@gmail.com>
2026-03-04 10:07:07 +0100

    feat(ios-common-cookies): add clearAllCookies function
    Hi there! 👋
    
    This pull request adds a new feature to the iOS Common Cookies module,
    allowing all cookies to be cleared from both `HTTPCookieStorage` and the
    WKWebView cookie store. The most important changes include the
    implementation of the new `clearAllCookies` method in both the native
    Swift module and the TypeScript interface, as well as the introduction
    of a new result type for this operation.
    
    **New Feature: Cookie Clearing**
    
    * Added a new async function `clearAllCookies` to the
    `iOSCommonCookiesModule` Swift class, which clears all cookies from
    `HTTPCookieStorage` and the WKWebView cookie store, and returns the
    number of cookies cleared from each.
    * Added the `clearAllCookies()` method to the TypeScript interface in
    `iOSCommonCookies.ts`, returning a `Promise<CookieClearResult>`.
    * Introduced a new `CookieClearResult` type in
    `iOSCommonCookies.types.ts` to represent the result of the cookie
    clearing operation, including counts for both stores.
    
    **TypeScript Typings Update**
    
    * Updated the import statement in `iOSCommonCookies.ts` to include the
    new `CookieClearResult` type.

--- packages/public/ios-common-cookies/src/iOSCommonCookies.ts
+++ packages/public/ios-common-cookies/src/iOSCommonCookies.ts
@@ -1,6 +1,6 @@
 import { NativeModule, requireNativeModule } from "expo-modules-core";
 
-import type { CookieBidirectionalSyncResult, CookieSyncResult } from "./iOSCommonCookies.types";
+import type { CookieBidirectionalSyncResult, CookieSyncResult, CookieClearResult } from "./iOSCommonCookies.types";
 
 declare class iOSCommonCookiesModule extends NativeModule {
   /** Copy cookies from HTTPCookieStorage -> WKWebsiteDataStore.default().httpCookieStore */
@@ -8,6 +8,9 @@ declare class iOSCommonCookiesModule extends NativeModule {
 
   /** Copy cookies from WKWebsiteDataStore.default().httpCookieStore -> HTTPCookieStorage */
   syncCookiesFromWebView(): Promise<CookieSyncResult>;
+
+  /** Clear all cookies from HTTPCookieStorage and WKWebView cookie store */
+  clearAllCookies(): Promise<CookieClearResult>;
 
   /** Best-effort two-way sync (HTTP -> WebView, then WebView -> HTTP) */
   syncCookiesBidirectional(): Promise<CookieBidirectionalSyncResult>;
